30697d864d9a5286d1a10b0df3e2381b788d3f30
Sibu77 sis <sisbusy99@gmail.com>
2023-11-21 20:40:46 +0000

    APOLLO DEDMO

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
-const { ApolloServer } = require("apollo-server");
-const { importSchema } = require("graphql-import");
-const EtherDataSource = require("./datasource/ethDatasource");
-const typeDefs = importSchema("./schema.graphql");
+const { ApolloServer } = require("apollo-server"); 
+// Import ApolloServer from apollo-server
 
-require("dotenv").config();
+const { importSchema } = require("graphql-import");
+// Import importSchema method from graphql-import
+
+const EtherDataSource = require("./datasource/ethDatasource");
+// Import EtherDataSource class 
+
+const typeDefs = importSchema("./schema.graphql");
+// Import GraphQL schemas
+
+require("dotenv").config(); 
+// Load environment variables from .env file
 
 const resolvers = {
   Query: {
-    getEthByAddress: (root, _args, { dataSources }) =>
+    etherBalanceByAddress: (root, _args, { dataSources }) => 
       dataSources.ethDataSource.etherBalanceByAddress(),
-    getTotalSupplyEth: (root, _args, { dataSources }) =>
+
+    totalSupplyOfEther: (root, _args, { dataSources }) =>
       dataSources.ethDataSource.totalSupplyOfEther(),
-    //Paste Code for New Resolver Functions
+
+    latestEthereumPrice: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.getLatestEthereumPrice(),
+
+    blockConfirmationTime: (root, _args, { dataSources }) =>  
+      dataSources.ethDataSource.getBlockConfirmationTime(),
   },
 };
+
+// Resolvers for GraphQL queries
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   dataSources: () => ({
     ethDataSource: new EtherDataSource(),
-  }),
+  }), 
 });
 
-server.timeout = 0;
+// Create Apollo Server instance
+
+server.timeout = 0; 
+
 server.listen("9000").then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
+
+// Start Apollo Server on port 9000
